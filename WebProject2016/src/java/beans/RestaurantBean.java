fe1c12d67d708c473a0f7aa2565cf41fc17b38da
Mirko Pani <panimirko95@gmail.com>
2016-10-19 14:31:41 +0200

    Refactoring of project folders and classes

---
+++ WebProject2016/src/java/beans/RestaurantBean.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author RiccardoUni
+ */
+public class RestaurantBean {
+    private int id;
+    private String name;
+    private String description;
+    private String web_site_url;
+    private int global_value;
+    private int id_owner;
+    private int id_creator;
+    private int id_price_range;
+    private Double latitude;
+    private Double longitude;
+    private String address;
+    private int cap;
+    private String city;
+    private String country;
+    private String telephone;
+
+    public String getE_mail() {
+        return e_mail;
+    }
+
+    public void setE_mail(String e_mail) {
+        this.e_mail = e_mail;
+    }
+    private String e_mail;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getWeb_site_url() {
+        return web_site_url;
+    }
+
+    public void setWeb_site_url(String web_site_url) {
+        this.web_site_url = web_site_url;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getGlobal_value() {
+        return global_value;
+    }
+
+    public void setGlobal_value(int global_value) {
+        this.global_value = global_value;
+    }
+
+    public int getId_owner() {
+        return id_owner;
+    }
+
+    public void setId_owner(int id_owner) {
+        this.id_owner = id_owner;
+    }
+
+    public int getId_creator() {
+        return id_creator;
+    }
+
+    public void setId_creator(int id_creator) {
+        this.id_creator = id_creator;
+    }
+
+    public int getId_price_range() {
+        return id_price_range;
+    }
+
+    public void setId_price_range(int id_price_range) {
+        this.id_price_range = id_price_range;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    } 
+    
+}
---
+++ WebProject2016/src/java/servlets/SearchAdvancedAutocomplete.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import com.google.gson.Gson;
+import com.sun.xml.internal.ws.util.StringUtils;
+import database.ManagerDB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utils.AutoCompleteData;
+
+/**
+ *
+ * @author MirkoPortatile
+ */
+@WebServlet(name = "SearchAdvancedAutocomplete", urlPatterns = {"/SearchAdvancedAutocomplete"})
+public class SearchAdvancedAutocomplete extends HttpServlet {
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String city = request.getParameter("city");
+        String country = request.getParameter("country");
+        String cuisine = request.getParameter("cuisine");
+        
+          List<String> valori = new ArrayList<String>();
+
+        //Instauriamo connessione
+        ManagerDB manager = new ManagerDB();
+        Connection connection = manager.getConnection();
+        
+        String sql="";
+        if(city != null)
+        {
+            sql="SELECT DISTINCT city as a from restaurants WHERE LOWER(city) like '%"+city.toLowerCase()+"%'";
+        }
+        else if(country != null)
+        {
+            sql="SELECT DISTINCT name as a from states WHERE LOWER(name) like '%"+country.toLowerCase()+"%'";
+        }
+        else if(cuisine!=null)
+        {
+            sql="SELECT DISTINCT name as a from cuisine WHERE LOWER(name) like '%"+cuisine.toLowerCase()+"%'";
+        }
+          try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet results = ps.executeQuery();
+
+            //Per tutti i risultati
+            while (results.next()) {
+                //Prendiamo una citta
+                String temp = StringUtils.capitalize(results.getString("a"));
+                    valori.add(temp);
+              
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchRestaurantAutocomplete.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Map real data into JSON
+        response.setContentType("application/json");
+
+        final List<AutoCompleteData> result = new ArrayList<AutoCompleteData>();
+        for (final String s : valori) {
+            result.add(new AutoCompleteData(s, s));
+        }
+        response.getWriter().write(new Gson().toJson(result));
+        
+    }
+
+    
+
+}
