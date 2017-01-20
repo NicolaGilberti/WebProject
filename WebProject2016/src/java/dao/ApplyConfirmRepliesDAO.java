9ec7ea333029a2957b70eeba4b2ce879edcafc5b
Marco Roberti <marco.roberti@studenti.unitn.it>
2017-01-20 01:48:40 +0100

    Untracked files commit

---
+++ WebProject2016/src/java/dao/ApplyConfirmRepliesDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import beans.ChangeOwnerNotificationBean;
+import beans.DeletePhotoNotificationBean;
+import beans.NotificationBean;
+import beans.NotificationRepliesBean;
+import database.ManagerDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Marco
+ */
+public class ApplyConfirmRepliesDAO {
+ ManagerDB db;
+    Connection con;
+    
+    public ApplyConfirmRepliesDAO(){
+        db= new ManagerDB();
+        con = db.getConnection();
+    }
+    
+    public int confirmReplies(int id) throws SQLException, Throwable{
+        PreparedStatement replies = con.prepareStatement("UPDATE replies SET accepted = true WHERE id =? ");
+        replies.setInt(1,id);
+        int value = replies.executeUpdate();
+        this.finalize();
+        con.close();
+        return value;
+    }
+    
+    public void discardReplies() throws SQLException, Throwable{
+        
+    }
+    //finalize per chiudere la connessione quando ho finito con la ricerca all'interno del database
+    @Override
+    protected void finalize() throws Throwable  
+{  
+    try { con.close(); } 
+    catch (SQLException e) { 
+        e.printStackTrace();
+    }
+    super.finalize();  
+}  
+    
+}
+
---
+++ WebProject2016/src/java/servlets/ConfirmRepliesServlet.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import dao.ApplyConfirmRepliesDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marco
+ */
+@WebServlet(name = "ConfirmRepliesServlet", urlPatterns = {"/ConfirmRepliesServlet"})
+public class ConfirmRepliesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, Throwable {
+            String button = null;
+            button = request.getParameter("a");
+            System.out.println(button);
+            int result = 0;
+            int id = Integer.parseInt(request.getParameter("id"));
+            ApplyConfirmRepliesDAO rep = new ApplyConfirmRepliesDAO();
+            if(button.equals("a")){
+                result = rep.confirmReplies(id);
+                 System.out.println("rip");
+            }
+            else{
+                System.out.println("rip");
+            }
+            
+            request.getSession().setAttribute("query_result",result);
+
+           response.sendRedirect("SearchNotification");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Throwable ex) {
+            Logger.getLogger(ConfirmRepliesServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Throwable ex) {
+            Logger.getLogger(ConfirmRepliesServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
