d68b211c510c0ec6978d218e07b003129e685684
Marco Roberti <marco.roberti@studenti.unitn.it>
2017-02-02 15:03:42 +0100

    admin bux fixing

--- WebProject2016/src/java/dao/AdminNotificationsDAO.java
+++ WebProject2016/src/java/dao/AdminNotificationsDAO.java
@@ -38,7 +38,7 @@ public class AdminNotificationsDAO {
     public NotificationBean getAllNotification() throws SQLException, Throwable{
         NotificationBean notification = new NotificationBean();
         //creo un preparedstatement e un rs per l'eseuzione della query e per recuperare il risultato della tabella
-        PreparedStatement replies = con.prepareStatement("SELECT t1.accepted AS accepted, t1.id AS idrep, t1.data_creation AS datarep,t2.data_creation AS revdata, t1.description AS repdes, t2.description AS rewdes, users.nickname AS customer,t4.nickname AS ristoratore FROM (replies AS t1 INNER JOIN reviews AS t2 ON (t1.id_review = t2.id)) INNER JOIN users ON (t2.id_creator = users.id) JOIN users AS t4 ON ( t1.id_owner = t4.id);");
+        PreparedStatement replies = con.prepareStatement("SELECT t1.accepted AS accepted, t1.id AS idrep, t1.data_creation AS datarep,t2.data_creation AS revdata, t1.description AS repdes, t2.description AS rewdes, users.nickname AS customer,t4.nickname AS ristoratore FROM (replies AS t1 INNER JOIN reviews AS t2 ON (t1.id_review = t2.id)) INNER JOIN users ON (t2.id_creator = users.id) JOIN users AS t4 ON ( t1.id_owner = t4.id) ORDER BY t1.data_creation DESC;");
         ResultSet rs = replies.executeQuery();
         try{
             try{    
@@ -58,7 +58,7 @@ public class AdminNotificationsDAO {
                 
                     
                 //recupero le notifiche del ChangeOwner
-                PreparedStatement cown = con.prepareStatement("SELECT t1.accepted AS accepted,t2.id AS resid,t3.id AS usrid , t2.name AS resname,t3.name AS usrname,t3.surname,t3.nickname, t4.nickname AS owner FROM ((request_changes_owner AS t1 INNER JOIN restaurants AS t2 ON (t1.id_restaurant =t2.id)) INNER JOIN users AS t3 ON (t1.id_user = t3.id)) INNER JOIN users AS t4 ON (t2.id_owner = t4.id)");
+                PreparedStatement cown = con.prepareStatement("SELECT t1.accepted AS accepted,t2.id AS resid,t3.id AS usrid , t2.name AS resname,t3.name AS usrname,t3.surname,t3.nickname, t4.nickname AS owner FROM ((request_changes_owner AS t1 INNER JOIN restaurants AS t2 ON (t1.id_restaurant =t2.id)) INNER JOIN users AS t3 ON (t1.id_user = t3.id)) INNER JOIN users AS t4 ON (t2.id_owner = t4.id);");
                 rs = cown.executeQuery();
                 while(rs.next()){
                     ChangeOwnerNotificationBean t1 = new ChangeOwnerNotificationBean();
